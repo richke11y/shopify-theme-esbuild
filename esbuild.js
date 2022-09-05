e130f270bc1d3fca9180772ab2bd5a4bdc11a15c
Rich Kelly <rich@richkelly.uk>
2022-09-05 13:26:34 +0100

    Updated require URL destination for entry-point-paths.js Added const variable to store project root string - passed into entryPointPaths() function.

--- esbuild.js
+++ esbuild.js
@@ -1,14 +1,16 @@
 const esbuild = require('esbuild');
 const sassPlugin = require('esbuild-plugin-sass');
-const { entryFilePaths } = require('./entry-file-paths.js');
+const { entryPointPaths } = require('./build-utilities/functions/entry-point-paths.js');
 const cleanAssetsDir = require('./build-utilities/plugins/clean-assets-dir.js');
 const copyStaticFiles = require('./build-utilities/plugins/copy-static-files.js');
 const watchStaticFiles = require('./build-utilities/plugins/watch-static-files.js');
 
+const projectRoot = __dirname;
+
 let config = {
 
 	mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
-	entryPoints: entryFilePaths(),
+	entryPoints: entryPointPaths(projectRoot),
 	outputDir: './assets/',
 	minify: process.env.NODE_ENV === 'development' ? false : true,
 	sourcemap: process.env.NODE_ENV === 'development' ? true : false,
@@ -16,10 +18,10 @@ let config = {
 	legalComments: process.env.NODE_ENV === 'development' ? 'inline' : 'none',
 	format: 'esm',
 	staticFileDirs: [
-		'./src/css/',
-		'./src/fonts/',
-		'./src/img/',
-		'./src/svg/'
+		`${projectRoot}/src/css/`,
+		`${projectRoot}/src/fonts/`,
+		`${projectRoot}/src/img/`,
+		`${projectRoot}/src/svg/`,
 	]
 
 }
