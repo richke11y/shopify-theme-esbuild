f1e04de9cdf979eda4a009b87516b9298c614c2f
Rich Kelly <rich@richkelly.uk>
2025-06-10 19:03:56 +0100

    Updated.

--- build-utilities/functions/entry-point-paths.js
+++ build-utilities/functions/entry-point-paths.js
@@ -5,31 +5,72 @@ const entryPointPaths = (root) => {
 
 	let entryPointsObj = {};
 
-	const getAllFiles = (dirPath, arrayFilePaths) => {
-	
-		const files = fs.readdirSync(dirPath)
-	  
+	const getAllFiles = (directoryPaths, arrayFilePaths) => {
+
 		arrayFilePaths = arrayFilePaths || []
-	
-		files.forEach((file) => {
-	
-			if (fs.statSync(`${dirPath}/${file}`).isDirectory()) {
-	
-				arrayFilePaths = getAllFiles(`${dirPath}/${file}`, arrayFilePaths);
-	
-			} else {
-	
-				arrayFilePaths.push(path.join(`${dirPath}/${file}`));
-	
+
+		directoryPaths.forEach((directoryPath) => {
+
+			let array = [];
+
+			const dirFiles = fs.readdirSync(directoryPath, { recursive: true });
+
+			for (let i = 0; i < dirFiles.length; i++) {
+
+				const file = dirFiles[i];
+				const filePath = `${directoryPath}/${file}`
+
+				if (fs.statSync(filePath).isDirectory()) continue;
+
+				array.push(filePath);
+
 			}
-		
+
+			// console.log(array);
+
+			arrayFilePaths = arrayFilePaths.concat(array);
+
 		});
 
+		// console.log(arrayFilePaths);
+	
 		return arrayFilePaths;
 	
 	}
 
-	const arrayFilePaths = getAllFiles(`${root}/src/js/templates`, []);
+	const arrayFilePaths = getAllFiles([
+		`${root}/src/js/templates`,
+		`${root}/src/js/sections`,
+		`${root}/src/js/web-components`
+	], []);
+
+	// const getAllFiles = (dirPath, arrayFilePaths) => {
+
+	// 	const files = fs.readdirSync(dirPath)
+	  
+	// 	arrayFilePaths = arrayFilePaths || []
+	
+	// 	files.forEach((file) => {
+	
+	// 		if (fs.statSync(`${dirPath}/${file}`).isDirectory()) {
+	
+	// 			arrayFilePaths = getAllFiles(`${dirPath}/${file}`, arrayFilePaths);
+	
+	// 		} else {
+	
+	// 			arrayFilePaths.push(path.join(`${dirPath}/${file}`));
+	
+	// 		}
+		
+	// 	});
+
+	// 	console.log(arrayFilePaths);
+
+	// 	return arrayFilePaths;
+	
+	// }
+
+	// const arrayFilePaths = getAllFiles(`${root}/src/js/templates`, []);
 
 	arrayFilePaths.forEach((filePath) => {
 
