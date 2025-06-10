232317658484f67a36c979aeda07e0295a0dc869
Rich Kelly <rich@richkelly.uk>
2025-06-10 19:06:02 +0100

    Importing reset.css

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,3 +1,10 @@
-import '../scss/theme.scss';
+import {name} from '../../package.json';
+import {version} from '../../package.json';
+import {author} from '../../package.json';
 
+import 'the-new-css-reset/css/reset.css';
+
+import '../scss/theme.scss';
 import './includes/header.js';
+
+window.onload = console.log(`%c ${name} (${version}) \n Web & Shopify Development: ${author}`,`background: black; color: white; text-transform: uppercase;`);
