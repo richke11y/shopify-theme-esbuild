025edff37510ec483ddce1f50bd071cc7af2d575
Rich Kelly <rich@richkelly.uk>
2025-09-30 14:23:55 +0100

    Updated.

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,8 +3,8 @@ import {version} from '../../package.json';
 import {author} from '../../package.json';
 
 import 'the-new-css-reset/css/reset.css';
-
 import '../scss/theme.scss';
-import './includes/header.js';
+
+import './global/header.js';
 
 window.onload = console.log(`%c ${name} (${version}) \n Web & Shopify Development: ${author}`,`background: black; color: white; text-transform: uppercase;`);
