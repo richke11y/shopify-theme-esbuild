5fd87531c15cd90f15a25cbf3fe255aa5eba260f
Rich Kelly <rich@richkelly.uk>
2025-09-30 14:20:16 +0100

    Compiled.

--- assets/bundle.404.js
+++ assets/bundle.404.js
@@ -1,2 +1,2 @@
-var o="shopify-theme-esbuild",i="0.0.3";var t="Rich Kelly <rich@richkelly.uk>";window.onload=console.log(`%c ${o} (${i}) 
- Web & Shopify Development: ${t}`,"background: black; color: white; text-transform: uppercase;");
+var o="shopify-theme-esbuild",i="0.0.3";var s="Rich Kelly <rich@richkelly.uk>";window.onload=console.log(`%c ${o} (${i}) 
+ Web & Shopify Development: ${s}`,"background: black; color: white; text-transform: uppercase;");
